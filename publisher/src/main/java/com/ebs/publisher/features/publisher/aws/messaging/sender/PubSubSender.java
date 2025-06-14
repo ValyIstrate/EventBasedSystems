773e2981e403eb7640b3e2170f674a7148a625ab
Valentin Istrate <valentin.istrate26@outlook.com>
2025-06-14 18:59:13 +0300

    SQS does not work yet, but the proto part can be used

--- publisher/src/main/java/com/ebs/publisher/features/publisher/aws/messaging/sender/PubSubSender.java
+++ publisher/src/main/java/com/ebs/publisher/features/publisher/aws/messaging/sender/PubSubSender.java
@@ -3,18 +3,18 @@ package com.ebs.publisher.features.publisher.aws.messaging.sender;
 import com.ebs.publisher.features.publisher.aws.messaging.message.PubSubMessage;
 import com.ebs.publisher.features.publisher.models.Publication;
 import com.ebs.publisher.features.publisher.models.Subscription;
+import com.ebs.publisher.features.publisher.proto_classes.MessageProto;
 import com.ebs.publisher.features.publisher.proto_classes.PublicationProto;
 import com.ebs.publisher.features.publisher.proto_classes.SubscriptionProto;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufFactory;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Slf4j
-@ConditionalOnMissingBean
 @Service
 public class PubSubSender extends BaseSender {
     private static final String PUBLICATION = "PubType";
@@ -23,29 +23,36 @@ public class PubSubSender extends BaseSender {
     @Value("${aws.sqs.all_broker_queue_name}")
     private String allBrokerQueueName;
 
-    private final ObjectMapper objectMapper;
+    private static final ProtobufMapper PROTOBUF_MAPPER = new ProtobufMapper(ProtobufFactory.builder().build());
 
-    public PubSubMessage<PublicationProto.PubProto> buildPublicationMessage(Publication publication) {
-        return new PubSubMessage<>(PUBLICATION, publication.buildProto());
+    public MessageProto.MsgProto buildPublicationMessage(Publication publication) {
+        var protoBuilder = MessageProto.MsgProto.newBuilder();
+        protoBuilder.setMessageType(PUBLICATION);
+        protoBuilder.setPublication(publication.buildProto());
+        return protoBuilder.build();
     }
 
-    public PubSubMessage<SubscriptionProto.SubProto> buildSubscriptionMessage(Subscription subscription) {
-        return new PubSubMessage<>(SUBSCRIPTION, subscription.buildProto());
+    public MessageProto.MsgProto buildSubscriptionMessage(Subscription subscription) {
+        var protoBuilder = MessageProto.MsgProto.newBuilder();
+        protoBuilder.setMessageType(SUBSCRIPTION);
+        protoBuilder.setSubscription(subscription.buildProto());
+        return protoBuilder.build();
     }
 
-    public PubSubSender(SqsClient sqsClient, ObjectMapper objectMapper) {
+    public PubSubSender(SqsClient sqsClient) {
         super(sqsClient);
-        this.objectMapper = objectMapper;
     }
 
     public void sendMessage(Object pubOrSub) {
         try {
             if (pubOrSub instanceof Publication) {
-                sendMessage(allBrokerQueueName,
-                        objectMapper.writeValueAsString(buildPublicationMessage((Publication) pubOrSub)));
+                MessageProto.MsgProto pubMessage = buildPublicationMessage((Publication) pubOrSub);
+                String serializedMessage = PROTOBUF_MAPPER.writeValueAsString(pubMessage);
+                sendMessage(allBrokerQueueName, serializedMessage);
             }  else if (pubOrSub instanceof Subscription) {
-                sendMessage(allBrokerQueueName,
-                        objectMapper.writeValueAsString(buildSubscriptionMessage((Subscription) pubOrSub)));
+                MessageProto.MsgProto subMessage = buildSubscriptionMessage((Subscription) pubOrSub);
+                String serializedMessage = PROTOBUF_MAPPER.writeValueAsString(subMessage);
+                sendMessage(allBrokerQueueName, serializedMessage);
             } else {
                 log.error("Unrecognized object type {}", pubOrSub.getClass());
             }
